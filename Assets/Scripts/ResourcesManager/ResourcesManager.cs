ea8b95ba5e200eed7e09f8c2afa84618f47b03e8
RainheartLang <ronaran@mail.ru>
2014-10-18 23:45:34 +0400

    2
    2

--- Assets/Scripts/ResourcesManager/ResourcesManager.cs
+++ Assets/Scripts/ResourcesManager/ResourcesManager.cs
@@ -2,16 +2,14 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
-
+//Стандартный менеджер ресурсов
 public class ResourcesManager {
 
     private const string TEXTURES_PATH = "Textures/";
 
-    private int a;
+    private Dictionary<Type, ResourceContainer> resourceContainers;  //список контейнеров ресурсов
 
-    private Dictionary<Type, ResourceContainer> resourceContainers;
-    private List<Texture2D> textures = new List<Texture2D>();
-
+    //инициализация, здесь добавляем все нужные контейнеры
     public void initialize()
     {
         this.resourceContainers = new Dictionary<Type, ResourceContainer>();
@@ -19,9 +17,10 @@ public class ResourcesManager {
         this.resourceContainers.Add(typeof(Texture2D), new ResourceContainer(typeof(Texture2D)));
     }
 
-
+    //Общий метод загрузки ресурса
     private T loadResource<T>(string path) where T: UnityEngine.Object
     {
+        //сначала ищем контейнер по типу дженерика
         Type resourceType = typeof(T);
         ResourceContainer container = resourceContainers[resourceType];
         if (container == null)
@@ -30,13 +29,14 @@ public class ResourcesManager {
             return null;
         }
 
+        //Пробуем найти ресурс в наших контейнерах
         T resource = (T)container.find(path);
 
         if (resource != null)
         {
             return resource;
         }
-
+        //Если не нашли - пытаемся загрузить через стандартный загрузчик
         resource = Resources.Load<T>(path);
 
         if (resource == null)
@@ -51,6 +51,7 @@ public class ResourcesManager {
         return resource;
     }
 
+    //получение ресурса-текстуры
     public Texture2D getTexture(String fileName)
     {
         return loadResource<Texture2D>(TEXTURES_PATH + fileName);
